2809fb919ee9dfd3b9a6fe2b73dc9c47ccae7559
Micheal A <aad3rinto@gmail.com>
2022-10-12 14:07:57 +0000

    add onclick functionality

---
+++ build/precache-manifest.76249c34d0b03b7f93d361d3aa9f2a71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9643ddf24c85a8a6946d169eabdec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90461fc51aa8e3aa881d",
+    "url": "/static/js/2.e8728cee.chunk.js"
+  },
+  {
+    "revision": "a3a83f33602a112f9b65",
+    "url": "/static/js/main.82929c46.chunk.js"
+  },
+  {
+    "revision": "cd0a330ff64fe6ff03a8",
+    "url": "/static/js/runtime-main.8a797268.js"
+  }
+]);
